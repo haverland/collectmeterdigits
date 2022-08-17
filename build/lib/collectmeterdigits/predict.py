9cd32e2bbb547ed13b333248a0724875ee6d378a
Frank Haverland <fspapaping@googlemail.com>
2022-08-17 16:10:40 +0200

    fix model option.predictions class11/10 models

--- build/lib/collectmeterdigits/predict.py
+++ build/lib/collectmeterdigits/predict.py
@@ -1,12 +1,15 @@
 import tensorflow as tf
 import numpy as np
 import pkg_resources
+from collectmeterdigits import glob
 
 interpreter=None
 internal_model_path = pkg_resources.resource_filename('collectmeterdigits', 'models/dig-s1-md-v1.2-q.tflite')
 
 def load_interpreter(model_path):
     global interpreter
+    print("Use model: " + model_path)
+    
     interpreter = tf.lite.Interpreter(model_path=model_path)
     return interpreter
 
@@ -14,7 +17,9 @@ def predict( image):
     global interpreter
 
     if interpreter==None:
-        load_interpreter(internal_model_path)
+        if glob.model_path==None:
+            glob.model_path=internal_model_path
+        load_interpreter(glob.model_path)
 
     interpreter.allocate_tensors()
     input_index = interpreter.get_input_details()[0]["index"]
@@ -27,6 +32,36 @@ def predict( image):
     # Run inference.
     interpreter.invoke()
     output = interpreter.get_tensor(output_index)
-    return np.argmax((output)[0])/10
+    print("Model=" + str(len(output[0])))
+    if (len(output[0])==11):
+        prediction = (np.argmax(output, axis=1).reshape(-1))[0]
+    else:
+        if (len(output[0])==10):
+            _num = (np.argmax(output, axis=1).reshape(-1))[0]
+            _numplus = (_num + 1) % 10
+            _numminus = (_num - 1 + 10) % 10
+
+            _val = output[0][_num]
+            _valplus = output[0][_numplus]
+            _valminus = output[0][_numminus]
+
+            result = _num
+
+            if (_valplus > _valminus):
+                result = result + _valplus / (_valplus + _val)
+                _fit = _val + _valplus
+            
+            else:
+            
+                result = result - _valminus / (_val + _valminus)
+                _fit = _val + _valminus
+            if (result >= 10):
+                result = result - 10
+            if (result < 0):
+                result = result + 10
+            prediction = result
+        else:
+            prediction = (np.argmax(output, axis=1).reshape(-1)/10)[0]
+    return prediction
         
     
