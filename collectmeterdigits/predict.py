3bbdcd626ebdca55c561d05892cf119dc170239c
Frank Haverland <fspapaping@googlemail.com>
2022-08-28 22:15:45 +0200

    optional tlfite and fix option model

--- collectmeterdigits/predict.py
+++ collectmeterdigits/predict.py
@@ -2,22 +2,38 @@ try:
     import tflite_runtime.interpreter as tflite
     has_tflite_runtime = True
 except ImportError:
-    has_tflite_runtime = False
-try:
-    import tensorflow.lite as tflite
-    has_tflite_runtime = True
-except ImportError:
-    has_tflite_runtime = False
+    try:
+        import tensorflow.lite as tflite
+        has_tflite_runtime = True
+    except ImportError:
+        has_tflite_runtime = False
 import numpy as np
 import pkg_resources
+from collectmeterdigits import glob
+
+interpreter=None
+internal_model_path = pkg_resources.resource_filename('collectmeterdigits', 'models/cnn32-md-20220621-002622-q.tflite')
+
+def load_interpreter(model_path):
+    global interpreter
+    print("Use model: " + model_path)
+    if (glob.model_path=="off"):
+        return
+    interpreter = tflite.Interpreter(model_path=model_path)
+    return interpreter
 
 
-def predict( image, model_path='cnn32-md-20220621-002622-q.tflite'):
 
-    if (has_tflite_runtime):
-        DATA_PATH = pkg_resources.resource_filename('collectmeterdigits', 'models/' + model_path)
+def predict( image):
+    global interpreter
 
-        interpreter = tflite.Interpreter(model_path=DATA_PATH)
+    if (has_tflite_runtime and not glob.model_path=="off" ):
+        
+        if interpreter==None:
+            if glob.model_path==None:
+                glob.model_path=internal_model_path
+            load_interpreter(glob.model_path)
+
         interpreter.allocate_tensors()
         input_index = interpreter.get_input_details()[0]["index"]
         output_index = interpreter.get_output_details()[0]["index"]
