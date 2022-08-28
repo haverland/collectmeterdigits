fc5b72e10fff318c218e37e2097fda213fdcd9a4
Frank Haverland <fspapaping@googlemail.com>
2022-08-28 21:39:20 +0200

    using tflite-runtime instead of tensorflow (optional now)

--- collectmeterdigits/predict.py
+++ collectmeterdigits/predict.py
@@ -1,20 +1,33 @@
-import tensorflow as tf
+try:
+    import tflite_runtime.interpreter as tflite
+    has_tflite_runtime = True
+except ImportError:
+    has_tflite_runtime = False
+try:
+    import tensorflow.lite as tflite
+    has_tflite_runtime = True
+except ImportError:
+    has_tflite_runtime = False
 import numpy as np
 import pkg_resources
 
 
 def predict( image, model_path='cnn32-md-20220621-002622-q.tflite'):
-    DATA_PATH = pkg_resources.resource_filename('collectmeterdigits', 'models/' + model_path)
 
-    interpreter = tf.lite.Interpreter(model_path=DATA_PATH)
-    interpreter.allocate_tensors()
-    input_index = interpreter.get_input_details()[0]["index"]
-    output_index = interpreter.get_output_details()[0]["index"]
+    if (has_tflite_runtime):
+        DATA_PATH = pkg_resources.resource_filename('collectmeterdigits', 'models/' + model_path)
 
-    interpreter.set_tensor(input_index, np.expand_dims(np.array(image).astype(np.float32), axis=0))
-    # Run inference.
-    interpreter.invoke()
-    output = interpreter.get_tensor(output_index)
-    return np.argmax((output)[0])/10
+        interpreter = tflite.Interpreter(model_path=DATA_PATH)
+        interpreter.allocate_tensors()
+        input_index = interpreter.get_input_details()[0]["index"]
+        output_index = interpreter.get_output_details()[0]["index"]
+
+        interpreter.set_tensor(input_index, np.expand_dims(np.array(image).astype(np.float32), axis=0))
+        # Run inference.
+        interpreter.invoke()
+        output = interpreter.get_tensor(output_index)
+        return np.argmax((output)[0])/10
+    else: 
+        return -1
         
     
