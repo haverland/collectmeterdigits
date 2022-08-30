b3368c8479ea615ba6132891b48e1bcd0d2b75b1
Frank Haverland <fspapaping@googlemail.com>
2022-08-30 13:35:36 +0200

    add packaging for application

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_description = """
 
 setup(
     name='collectmeterdigits',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/haverland/collectmeterdigits',
     license='Apache 2.0',
     author='Frank Haverland',
